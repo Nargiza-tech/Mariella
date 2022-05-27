6576c141f7217b2f468d8c10175bb1060c471a01
Nargiza <saparbaeva.nargiza@gmail.com>
2022-05-27 09:49:54 +0600

    add project Mariella

---
+++ app.js
@@ -0,0 +1,19 @@
+secondsAMinute = 60;
+minutesInAHours = 60;
+secondIsAHour = secondsAMinute * minutesInAHours;
+
+console.log(secondIsAHour);
+
+hoursIsADay = 24;
+
+seconIsADay = secondIsAHour * hoursIsADay;
+
+console.log(seconIsADay);
+
+dayIsInYear = 365;
+
+
+secondIsYear = seconIsADay * dayIsInYear;
+
+console.log(secondIsYear)
+
